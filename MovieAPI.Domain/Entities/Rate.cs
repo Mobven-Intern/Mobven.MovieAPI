dc322ee865bf3e547e167ee0101a8c24a5d6946c
doathe <nisa.tur@gmail.com>
2024-04-18 17:35:47 +0300

    entities created; dependencies added

---
+++ MovieAPI.Domain/Entities/Rate.cs
@@ -0,0 +1,11 @@
+﻿using MovieAPI.Domain.Entities.Common;
+
+namespace MovieAPI.Domain.Entities;
+
+public class Rate : BaseEntity
+{
+    public float Rating { get; set; }
+
+    public DateTime CreatedOn { get; set; }
+    public DateTime UpdatedOn { get; set; }
+}
